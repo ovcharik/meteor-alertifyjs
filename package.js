a6c64a923ae3f82c858620311920da27e09299f3
ovcharik <maksim.ovcharik@gmail.com>
2015-03-07 21:31:03 +0500

    v1.2.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ovcharik:alertifyjs',
   summary: 'A javascript framework for developing pretty browser dialogs and notifications.',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/ovcharik/meteor-alertifyjs'
 });
 
